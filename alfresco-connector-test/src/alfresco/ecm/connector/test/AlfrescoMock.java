8bb075596ff45fe72ddbfc6ba4d55179e1c0828f
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-04-28 16:13:09 +0200

    do not use environments for testing
    - feature no longer exists on 11.1 onwards

--- alfresco-connector-test/src/alfresco/ecm/connector/test/AlfrescoMock.java
+++ alfresco-connector-test/src/alfresco/ecm/connector/test/AlfrescoMock.java
@@ -9,18 +9,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 import org.apache.commons.io.IOUtils;
+
 import io.swagger.v3.oas.annotations.Hidden;
 
 /**
  * Mocks parts of the Alfresco ECM Rest API which has been used in the AlfrescoEcmDemo
  * <p>docs<br/>https://api-explorer.alfresco.com/api-explorer/definitions/alfresco-core.json</p>
  */
-@Path("ecmMock")
+@Path(AlfrescoMock.PATH_SUFFIX)
 @PermitAll
 @Hidden
 public class AlfrescoMock
-{ 
+{
+  static final String PATH_SUFFIX = "ecmMock";
+
+  // URI where this mock can be reached: to be referenced in tests that use it!
+  @SuppressWarnings("restriction")
+  public static final String URI = "{"+ch.ivyteam.ivy.rest.client.config.IvyDefaultJaxRsTemplates.APP_URL+"}/api/"+PATH_SUFFIX;
+
+
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("nodes/-shared-/children")
@@ -28,7 +37,7 @@ public class AlfrescoMock
   {
     return load("json/rootfolder.json");
   }
-  
+
   private static String load(String path)
   {
     try(InputStream is = AlfrescoMock.class.getResourceAsStream(path))
